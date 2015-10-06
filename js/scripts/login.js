b5534fa1404363dacbd94975698019b9da90c592
miche <michelle_TAN@ida.gov.sg>
2015-10-06 11:55:46 +0800

    added login check for challenge.html;

--- js/scripts/login.js
+++ js/scripts/login.js
@@ -1,4 +1,14 @@
 function loginUsingSingpass() {
-	localStorage["user"] = 'CHARLES.PCTL';
+	localStorage["isLoggedIn"] = true;
 	window.location.href = "challenge.html";
+}
+
+function verifyIfIsLoggedIn() {
+	if(localStorage["isLoggedIn"]) {
+		$('#isLoggedIn').show();
+		$('#isLoggedOut').hide();
+	} else {
+		$('#isLoggedIn').hide();
+		$('#isLoggedOut').show();		
+	}
 }
