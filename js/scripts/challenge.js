87a930de9edc4d6f9196a102e1ab7e576c4d8ce7
miche <michelle_TAN@ida.gov.sg>
2015-10-07 09:26:31 +0800

    hide/show confirm prompt;

--- js/scripts/challenge.js
+++ js/scripts/challenge.js
@@ -2,6 +2,8 @@ $(function () {
 	var path = window.location.search.split('?s=');
 	if(path[1] == '1') {
 		//show submission confirmed
-		
+		$('#confirmSubmit').show();
+	}else {
+		$('#confirmSubmit').hide();
 	}
 });
