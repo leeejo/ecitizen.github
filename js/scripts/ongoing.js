a4ba22690c4fff1a2fb64ed93f504e27e1629ac4
miche <michelle_TAN@ida.gov.sg>
2015-10-07 17:41:28 +0800

    added redirect script to submit your idea button;

--- js/scripts/ongoing.js
+++ js/scripts/ongoing.js
@@ -1,9 +1,19 @@
-$(function () {
+$(function() {
 	var path = window.location.search.split('?s=');
-	if(path[1] == '1') {
+	if (path[1] == '1') {
 		//show submission confirmed
 		$('#confirmSubmit').show();
-	}else {
+	} else {
 		$('#confirmSubmit').hide();
 	}
 });
+
+function goToSubmit() {
+	if (localStorage["isLoggedIn"]) {
+		//go to submit
+		window.location.href = "submit.html";
+	} else {
+		//go to login page
+		window.location.href = "login.html";
+	}
+}
